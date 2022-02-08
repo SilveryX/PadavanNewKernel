db126e604331adb4ea44087c0b291279f834e9f6
SilveryX <67977075+SilveryX@users.noreply.github.com>
2022-02-08 11:41:43 +0800

    Update board.mk

--- trunk/configs/boards/R6800/board.mk
+++ trunk/configs/boards/R6800/board.mk
@@ -4,6 +4,6 @@
 # NETGEAR-BZV # NETGEAR-BZV   # MT7621  #
 ##################################################################
 
-CFLAGS += -DBOARD_NETGEAR_BZV
+CFLAGS += -DBOARD_R6800
 BOARD_NUM_USB_PORTS=2
 CONFIG_BOARD_RAM_SIZE=256
